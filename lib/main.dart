b510a81887e9cf2896810569881392b594076a2f
HugoLaurent <lauhugopro@gmail.com>
2024-01-26 20:02:46 +0100

    Remove commented code and update text theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 import "package:expense_tracker/widgets/expenses.dart";
 
@@ -13,56 +13,56 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]).then((fn) => {
-        runApp(
-          MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: const Expenses(),
-            darkTheme: ThemeData.dark().copyWith(
-              colorScheme: kDarkColorScheme,
-              cardTheme: const CardTheme().copyWith(
-                color: kDarkColorScheme.secondaryContainer,
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 8,
-                ),
-              ),
-              elevatedButtonTheme: ElevatedButtonThemeData(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: kDarkColorScheme.primaryContainer,
-                ),
-              ),
-            ),
-            theme: ThemeData().copyWith(
-              colorScheme: kColorScheme,
-              appBarTheme: const AppBarTheme().copyWith(
-                backgroundColor: kColorScheme.onPrimaryContainer,
-                foregroundColor: kColorScheme.primaryContainer,
-              ),
-              cardTheme: const CardTheme().copyWith(
-                color: kColorScheme.secondaryContainer,
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 8,
-                ),
-              ),
-              elevatedButtonTheme: ElevatedButtonThemeData(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: kColorScheme.primaryContainer,
-                ),
-              ),
-              textTheme: ThemeData().textTheme.copyWith(
-                    titleLarge: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: kColorScheme.onSecondaryContainer,
-                      fontSize: 16,
-                    ),
-                  ),
-            ),
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) => {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Expenses(),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
           ),
-        )
-      });
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+          ),
+        ),
+      ),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16,
+              ),
+            ),
+      ),
+    ),
+  );
+  // });
 }
